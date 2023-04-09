60dedb3ed558252b7b85c87a5751eba7b713db51
Carlos <offbyte@gmail.com>
2023-04-09 11:35:10 +0100

    Migrating to Gradle Kotlin DSL

--- app/src/main/java/io/monteirodev/calorietracker/ui/theme/Theme.kt
+++ app/src/main/java/io/monteirodev/calorietracker/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = BrightGreen,
@@ -31,7 +32,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun CaloryTrackerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun CalorieTrackerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
