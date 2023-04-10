f1aa0d8cb5bbc72d133d24833f7e7f60e0d75190
Carlos <offbyte@gmail.com>
2023-04-10 22:33:03 +0100

    Gender Screen

--- app/src/main/java/io/monteirodev/calorietracker/MainActivity.kt
+++ app/src/main/java/io/monteirodev/calorietracker/MainActivity.kt
@@ -8,11 +8,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
 import io.monteirodev.calorietracker.navigation.navigate
 import io.monteirodev.calorietracker.ui.theme.CalorieTrackerTheme
 import io.monteirodev.core.navigation.Route
+import io.monteirodev.onboarding_presentation.gender.GenderScreen
 import io.monteirodev.onboarding_presentation.welcome.WelcomeScreen
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +33,7 @@ class MainActivity : ComponentActivity() {
 
                     }
                     composable(Route.GENDER) {
-
+                        GenderScreen(onNavigate = navController::navigate)
                     }
                     composable(Route.HEIGHT) {
 
