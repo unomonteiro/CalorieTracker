af88c5630dd9c4f47beb7522c7700bfb65262c4c
Carlos Monteiro <offbyte@gmail.com>
2023-05-19 22:28:08 +0300

    TrackerOverviewViewModel

---
+++ tracker/tracker_presentation/src/main/java/io/monteirodev/tracker_presentation/tracker_overview/TrackerOverviewEvent.kt
@@ -0,0 +1,11 @@
+package io.monteirodev.tracker_presentation.tracker_overview
+
+import io.monteirodev.tracker_domain.model.TrackedFood
+
+sealed class TrackerOverviewEvent {
+    object OnNextDayClick: TrackerOverviewEvent()
+    object OnPreviousDayClick: TrackerOverviewEvent()
+    data class OnToggleMealClick(val meal: Meal): TrackerOverviewEvent()
+    data class OnDeleteTrackedFoodClick(val trackedFood: TrackedFood): TrackerOverviewEvent()
+    data class OnAddFoodClick(val meal: Meal): TrackerOverviewEvent()
+}
